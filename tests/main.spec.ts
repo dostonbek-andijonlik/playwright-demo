3e29e7ee8eca19c50f65e4cc4d3b91ebda6b607e
dostonbek.andijonlik <dostonbek.andijonlik@gmail.com>
2025-12-09 21:48:46 +0500

    First playwright test

---
+++ tests/main.spec.ts
@@ -0,0 +1,8 @@
+import {test, expect} from "@playwright/test"
+
+test("Verify page title", async ({page}) => {
+    await page.goto("https://google.com")
+    let title: string = await page.title()
+    console.log("Title is: ", title);
+    await expect(page).toHaveTitle("Google")
+})
